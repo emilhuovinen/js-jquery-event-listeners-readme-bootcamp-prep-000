4a422e3c6159e5e088e794679e2904d44b9684ec
emil.huo <emil.huo@gmail.com>
2018-10-01 19:27:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,7 +10,7 @@ function frameIt () {
 }
 
 function pressIt () {
-  $('input').on('keydown', function(){
+  $('input').on('keydown', function(key){
     if (key.which == 71) {
       alert('You have press G')
     }
